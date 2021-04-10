0a5ea2e944866ad8d65a94f0c728a1cd24bf385c
Vanessa <vsnakahara@gmail.com>
2021-04-10 12:26:30 -0300

    Organização de estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: "", redirectTo: "home", pathMatch: "full" }];
+const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HttpClientModule],
